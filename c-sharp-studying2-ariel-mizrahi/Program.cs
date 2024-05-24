20d74e30d1f88acc5e6f7ce8a9f0db17811b9b38
visual <visual@hermelin.ort.org.il>
2024-05-24 09:59:58 +0300

    Add project files.

---
+++ c-sharp-studying2-ariel-mizrahi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_sharp_studying2_ariel_mizrahi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
