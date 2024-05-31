364aac85d04473b8b05da5fd01f8693b1ab9272a
ariel mizrahu <arimiz5928@gmail.com>
2024-05-31 10:10:07 +0300

    change

---
+++ c-sharp-studying2-ariel-mizrahi/bacic-object/new file.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_studying2_ariel_mizrahi.bacic_object
+{
+    internal class new_file
+    {
+        int i = 1;
+    }
+}
